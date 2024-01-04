b016b264015ae17be350ba8ca1c88b132a4e4360
arthurmassimetti <135754837+arthurmassimetti@users.noreply.github.com>
2024-01-04 17:16:07 -0300

    Update desafio4.js

--- desafio4.js
+++ desafio4.js
@@ -5,6 +5,6 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14,]
 for (let i = 0; i < numeros.length; i++) {
    const numeroPar = numeros[i]   
    if (numeroPar %2===0){
-    console.log(numeroPar)
+    console.log(numeroPar "este é seu numero")
    }
 }
